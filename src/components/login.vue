bbeb1d7719237f257627d441ce2182e1a361eda6
ZenithNUC <lizengzhi1998@foxmail.com>
2021-07-17 15:50:05 +0800

    add:绘制了登陆表单

--- src/components/login.vue
+++ src/components/login.vue
@@ -4,6 +4,22 @@
       <div class="avatar_box">
         <img src="../assets/img/logo.png" alt="">
       </div>
+      <!--登陆表单-->
+      <el-form label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input prefix-icon="el-icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input type="password" prefix-icon="el-icon-lock"></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btn">
+          <el-button type="primary">登陆</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
@@ -31,15 +47,30 @@ export default {
     .avatar_box{
       height: 130px;
       width: 130px;
+      background-color: #81ecec;
       border:5px solid #81ecec;
       border-radius: 50%;
       padding: 10px;
       box-shadow: 0 0 10px #ddd;
+      position: absolute;
+      left: 50%;
+      top: -25%;
+      transform: translate(-50%);
       img{
         width: 100%;
         height: 100%;
         border-radius: 50%;
       }
     }
+  }
+  .btn{
+    text-align: right;
+  }
+  .login_form{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing:border-box;
   }
 </style>
