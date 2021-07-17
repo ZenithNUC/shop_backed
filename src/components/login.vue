2b0c17151c6b34377895dbc9e6357dfae55b3e63
ZenithNUC <lizengzhi1998@foxmail.com>
2021-07-17 11:14:15 +0800

    add:增加了登录页样式

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    login
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/img/logo.png" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +15,31 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+  .login_container{
+    background-color: #2b4b6b;
+    height: 100%;
+  }
+  .login_box{
+    width: 450px;
+    height: 300px;
+    background-color: #ffffff;
+    border-radius: 10px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    margin: -150px 0 0 -225px;
+    .avatar_box{
+      height: 130px;
+      width: 130px;
+      border:5px solid #81ecec;
+      border-radius: 50%;
+      padding: 10px;
+      box-shadow: 0 0 10px #ddd;
+      img{
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+      }
+    }
+  }
 </style>
